b8030cdf2846f42f73d16f8dfa52932f6262d749
ShTanyaa <124123694+ShTanyaa@users.noreply.github.com>
2023-02-01 22:31:52 +0500

    commit

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Program
+    {
+        public class Tel
+        {
+            public int day;
+            public double time;
+            public double price;
+            public double CalculateSum()
+            {
+                double totalPrice = time * price;
+                return day == 6 || day == 7 ? totalPrice - totalPrice * 0.1 : totalPrice;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+            Tel tel = new Tel();
+            Console.WriteLine("vvedite day razgovora");
+            tel.day = int.Parse(Console.ReadLine());
+            Console.WriteLine("vvedite vrema razgovora");
+            tel.time = int.Parse(Console.ReadLine()); ;
+            Console.WriteLine("vvedite cost razgovora");
+            tel.price = int.Parse(Console.ReadLine()); ;
+            Console.WriteLine(tel.CalculateSum());
+
+            }
+            catch
+            {
+                Console.WriteLine("error");
+            }
+            
+
+        }
+    }
+}
---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        public class cycle
+        {
+            public void ckl()
+            {
+                int square = 0;
+                for (int i = 1; i <= 10; i++)
+                {
+                    square = i * i;
+                    Console.WriteLine(i);
+                    print pr = new print();
+                    pr.printt(square);
+                }
+            }
+        }
+
+        public class print
+        {
+            public void printt(int arg)
+            {
+                if (arg > 25)
+                    Console.WriteLine("Квадрат числа больше 25!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            cycle zic = new cycle();
+            zic.ckl();
+            Console.Read();
+        }
+    }
+}
